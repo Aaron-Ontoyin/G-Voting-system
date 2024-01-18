39d87c773eb8966a24bce39b24fbc3fef5b1d6ae
Aaron Ontoyin Yin <aarononto909@gmail.com>
2024-01-18 08:23:47 +0000

    Make caption bolder

--- app.py
+++ app.py
@@ -36,8 +36,9 @@ if "other" not in st.session_state:
     st.session_state["other"] = []
 
 
-st.image(r"bg.webp", caption="GRIDCo Senior Staff Association", use_column_width=True)
-st.write("")
+st.image(r"bg.webp", use_column_width=True)
+st.markdown("""<h5>GRIDCo Senior Staff Association Digital Platform</h5>""", unsafe_allow_html=True)
+st.write()
 
 valid_ids = pd.read_csv("staff_ids.csv")["Staffid"].astype(str).to_list()
 
